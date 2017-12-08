3b599446b530f31c540cc850c7ea06a31ae708a5
hoahiepnguyen <hiep@olli-ai.com>
2017-12-08 15:32:45 +0700

    kdfld

--- sound/soc/davinci/davinci-evm.c
+++ sound/soc/davinci/davinci-evm.c
@@ -479,7 +479,6 @@ static struct snd_soc_dai_link evm_dai_tlv320aic32x4 = {
 	.stream_name 	= "AIC32X",
 	.codec_dai_name = "tlv320aic32x4-hifi",
 	.ops 			= &evm_ops,
-//	.init 			= 
 	.dai_fmt = SND_SOC_DAIFMT_DSP_B | SND_SOC_DAIFMT_CBM_CFM |
 		   SND_SOC_DAIFMT_IB_NF,
 };
